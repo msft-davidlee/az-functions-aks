d26ea4ae80c860f9a1e747ac515884d9bbfe7e84
David Lee <leedavid@microsoft.com>
2021-09-17 13:14:52 -0500

    update load test

--- Test/LoadTest.ps1
+++ Test/LoadTest.ps1
@@ -1,4 +1,4 @@
-param([int]$Seconds = 60, [int]$Intensity = 5)
+param([int]$Seconds = 60, [int]$Intensity = 5, $TestApi)
 
 $report = @{
     items = @()
@@ -6,17 +6,32 @@ $report = @{
 for ($x = 0; $x -lt $Intensity; $x++) { 
     # Start Parallel Jobs
     Start-Job -ScriptBlock {
-        param($Seconds, $Report)        
+        param($Seconds, $Report, $TestApi)        
+        
         $current = Get-Date
         $total = 0
         $counter = 0
+        $url = "http://contosoapi.com"
+
         while ($true) {
             
             $end = Get-Date
             $diff = New-TimeSpan -Start $current -End $end
 
             for ($i = 0; $i -lt 1000; $i++) {
-                $total += (Measure-Command -Expression { Invoke-RestMethod -UseBasicParsing -Uri http://contosoapi.com/ping -Method Post; }).Milliseconds
+                $total += (Measure-Command -Expression { 
+
+                        if ($TestApi -eq "todo") {
+                            $description = (New-Guid).ToString().Replace("-", " ")
+                            $body = @{ description = $description; }
+    
+                            Invoke-RestMethod -UseBasicParsing -Uri ($url + "/todo") -Body ($body | ConvertTo-Json) -Method Post
+                        }
+
+                        if ($TestApi -eq "ping") {
+                            Invoke-RestMethod -UseBasicParsing -Uri ($url + "/ping") -Method Post;
+                        }                                                                        
+                    }).Milliseconds
                 $counter += 1
             }
 
@@ -28,7 +43,7 @@ for ($x = 0; $x -lt $Intensity; $x++) {
         $avg = $total / $counter
         $Report.items += $avg
 
-    } -ArgumentList @($Seconds, $report)
+    } -ArgumentList @($Seconds, $report, $TestApi)
 }
 
 Get-Job
