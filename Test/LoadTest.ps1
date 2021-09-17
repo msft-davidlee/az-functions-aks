26fd41d906083c8f701ae0677bf4e0adbf9d9865
David Lee <leedavid@microsoft.com>
2021-09-17 11:52:32 -0500

    disable authentication

--- Test/LoadTest.ps1
+++ Test/LoadTest.ps1
@@ -1,17 +1,23 @@
 param([int]$Seconds = 60, [int]$Intensity = 5)
 
+$report = @{
+    items = @()
+}
 for ($x = 0; $x -lt $Intensity; $x++) { 
     # Start Parallel Jobs
     Start-Job -ScriptBlock {
-        param($Seconds)        
+        param($Seconds, $Report)        
         $current = Get-Date
+        $total = 0
+        $counter = 0
         while ($true) {
             
             $end = Get-Date
             $diff = New-TimeSpan -Start $current -End $end
 
             for ($i = 0; $i -lt 1000; $i++) {
-                Invoke-RestMethod -UseBasicParsing -Uri http://contosoapi.com/ping -Method Post; 
+                $total += (Measure-Command -Expression { Invoke-RestMethod -UseBasicParsing -Uri http://contosoapi.com/ping -Method Post; }).Milliseconds
+                $counter += 1
             }
 
             if ($diff.Seconds -gt $Seconds) {
@@ -19,5 +25,11 @@ for ($x = 0; $x -lt $Intensity; $x++) {
             }
         }
 
-    } -ArgumentList $Seconds
+        $avg = $total / $counter
+        $Report.items += $avg
+
+    } -ArgumentList @($Seconds, $report)
 }
+
+Get-Job
+$report
